21dd32ce9111cde86de7cd289d114aa1de7c052d
golfingirl54 <alexandra@riskbazaar.org>
2017-06-04 11:32:16 -0700

    Edit contact php file removing headers from mail function

--- client/mail/contact_me.php
+++ client/mail/contact_me.php
@@ -20,8 +20,9 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'schwenter@dlt.education'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nCompany: $company\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
+// $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+// $headers .= "Reply-To: $email_address";   
+// mail($to,$email_subject,$email_body,$headers);
+mail($to,$email_subject,$email_body);
 return true;         
 ?>
